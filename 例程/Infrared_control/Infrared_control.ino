82216d6fa8cfb81601da450d62e13ad86e9e200d
五丝菜卷 <chenjiegd@foxmail.com>
2019-06-05 15:09:15 +0800

    Update Infrared_control.ino

--- 例程/Infrared_control/Infrared_control.ino
+++ 例程/Infrared_control/Infrared_control.ino
@@ -33,14 +33,39 @@
 #define R_Motor_PIN1 9  //后
 #define R_Motor_PIN2 6  //前
 
+uint8_t i;
+int buzzer = 12; //设置控制蜂鸣器的引脚
+int blink = 13;
+int key = 8; //按键key
+
 int RECV_PIN = 2; //定义红外接收器的引脚为2
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-int on = 0;//标志位
+int on = 0; //标志位
 unsigned long last = millis();
 
-long flag_run = 
+long flag_switch = 0xFF00FF;	 //开关
+long flag_run = 0xFF807F;		 //前进
+long flag_led = 0xFF40BF;		 //灯
+long flag_left = 0xFF20DF;		 //左转
+long flag_buzzer = 0xFFA05F;	 //喇叭
+long flag_right = 0xFF609F;		 //右转
+long flag_spin_left = 0xFF10EF;  //左旋
+long flag_back = 0xFF906F;		 //后退
+long flag_spin_right = 0xFF50AF; //右旋
+long flag_plus = 0xFF30CF;		 //加号
+long flag_zero = 0xFFB04F;		 //0
+long flag_minus = 0xFF708F;		 //减号
+long flag_one = 0xFF08F7;		 //1
+long flag_two = 0xFF8877;		 //2
+long flag_three = 0xFF48B7;		 //3
+long flag_four = 0xFF28D7;		 //4
+long flag_five = 0xFFA857;		 //5
+long flag_six = 0xFF6897;		 //6
+long flag_seven = 0xFF18E7;		 //7
+long flag_eight = 0xFF9867;		 //8
+long flag_nine = 0xFF58A7;		 //9
 
 void setup()
 {
@@ -57,16 +82,113 @@ void setup()
 void loop()
 {
 	// put your main code here, to run repeatedly:
-	if (irrecv.decode(&results))
+	// if (irrecv.decode(&results))
+	// {
+	// 	Serial.print("irCode: ");
+	// 	Serial.print(results.value, HEX); //以16进制换行输出接收代码
+	// 	// Serial.println();					//为了便于观看输出结果增加一个空行
+	// 	//results.value 是unsigned long型，头文件有介绍
+	// 	delay(100); //按键消抖
+	// 	Serial.print(", bits: ");
+	// 	Serial.println(results.bits); // 红外线码元位数
+	// 	irrecv.resume();			  // 接收下一个值
+	// }
+
+	if (irrecv.decode(&results)) //调用库函数：解码
 	{
-		Serial.print("irCode: ");
-		Serial.print(results.value, HEX); //以16进制换行输出接收代码
-		// Serial.println();					//为了便于观看输出结果增加一个空行
-		//results.value 是unsigned long型，头文件有介绍
-		delay(100); //按键消抖
-		Serial.print(", bits: ");
-		Serial.println(results.bits); // 红外线码元位数
-		irrecv.resume();			  // 接收下一个值
+		if (millis() - last > 250) //确定接收到信号
+		{
+			on = !on; //标志位置反
+			digitalWrite(13, on ? HIGH : LOW);
+			dump(&results);
+		}
+		if (results.value == flag_switch)
+		{
+			/* code */
+		}
+		else if (results.value == flag_run)
+		{
+			/* code */
+		}
+		else if (results.value == flag_led)
+		{
+			/* code */
+		}
+		else if (results.value == flag_left)
+		{
+			/* code */
+		}
+		else if (results.value == flag_buzzer)
+		{
+			/* code */
+		}
+		else if (results.value == flag_right)
+		{
+			/* code */
+		}
+		else if (results.value == flag_spin_left)
+		{
+			/* code */
+		}
+		else if (results.value == flag_back)
+		{
+			/* code */
+		}
+		else if (results.value == flag_spin_right)
+		{
+			/* code */
+		}
+		else if (results.value == flag_plus)
+		{
+			/* code */
+		}
+		else if (results.value == flag_zero)
+		{
+			/* code */
+		}
+		else if (results.value == flag_minus)
+		{
+			/* code */
+		}
+		else if (results.value == flag_one)
+		{
+			/* code */
+		}
+		else if (results.value == flag_two)
+		{
+			/* code */
+		}
+		else if (results.value == flag_three)
+		{
+			/* code */
+		}
+		else if (results.value == flag_four)
+		{
+			/* code */
+		}
+		else if (results.value == flag_five)
+		{
+			/* code */
+		}
+		else if (results.value == flag_six)
+		{
+			/* code */
+		}
+		else if (results.value == flag_seven)
+		{
+			/* code */
+		}
+		else if (results.value == flag_eight)
+		{
+			/* code */
+		}
+		else if (results.value == flag_nine)
+		{
+			/* code */
+		}
+		else
+		{
+		}
 	}
 }
 
@@ -242,4 +364,14 @@ void stop()
 	digitalWrite(R_Motor_PIN2, LOW);
 	analogWrite(R_Motor_PIN1, 0);
 	analogWrite(R_Motor_PIN2, 0);
+}
+
+void dump(decode_results *results)
+{
+	int count = results->rawlen;
+	if (results->decode_type == UNKNOWN)
+	{
+		//Serial.println("Could not decode message");
+		stop();
+	}
 }
