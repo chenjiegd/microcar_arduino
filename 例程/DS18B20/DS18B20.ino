4040ae5b9f5fab87b8db805312d11ef7fc010eb3
五丝菜卷 <chenjiegd@foxmail.com>
2019-06-05 10:50:30 +0800

    Create DS18B20.ino

---
+++ 例程/DS18B20/DS18B20.ino
@@ -0,0 +1,20 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 8            // 定义DS18B20数据口连接arduino的 2 脚
+OneWire oneWire(ONE_WIRE_BUS);    // 初始连接在单总线上的单总线设备
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);             // 设置串口通信波特率
+  sensors.begin();                // 初始库
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensors.requestTemperatures();  // 发送命令获取温度
+  Serial.print("温度值：");          //串口打印温度值
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.println("℃");
+  delay(10);
+}
